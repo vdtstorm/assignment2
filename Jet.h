dafd07f2485f9e6f50232364de09befb6d84c323
Vinson Thomas <vdt16@my.fsu.edu>
2019-09-30 03:27:47 -0400

    Creation of header file "Jet.h" for Driving simulator class

---
+++ Jet.h
@@ -0,0 +1,28 @@
+// vinson Thomas(vdt16)
+// 9/30/19
+
+#ifndef DRIVINGSIMULATOR_JET_H
+#define DRIVINGSIMULATOR_JET_H
+
+#include  "PoweredVehicle.h"
+
+
+class Jet : public PoweredVehicle
+{
+	private:
+		int numberOfEngines;
+
+	public:
+		explicit Jet(string brand, string model, string fuelType, int engineNumber = 1,);
+
+	virtual ~Jet();
+	int getEngineNumber();
+	void setEngineNumber();
+	virtual double mileageEstimate(double time);
+	virtual string toString();
+
+
+
+};
+
+#endif
