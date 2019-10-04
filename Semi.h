009e58c463ec592290a0266330ee1ff088b3dd6f
Vinson Thomas <vdt16@my.fsu.edu>
2019-10-04 02:30:43 -0400

    Declared functions and member data for Semi.h

---
+++ Semi.h
@@ -0,0 +1,24 @@
+#ifndef DRIVINGSIMULATOR_SEMI_H
+#define DRIVINGSIMULATOR_SEMI_H
+
+#include "PoweredVehicle.h"
+
+class Semi : public PoweredVehicle
+{
+	private:
+    	string myEngineSize;
+
+	public:
+    	Semi();
+
+    	explicit Semi(string brand, string model, string fuelType,
+                 string engineSize);
+
+    	virtual ~Semi();
+    	string getEngineSize();
+    	void setEngineSize(string engineSize);
+    	virtual double mileageEstimate(double time);
+    	virtual string toString();
+};
+
+#endif
