c83a70186966ca79deaabb962367cd98d83f6175
Vinson Thomas <vdt16@my.fsu.edu>
2019-10-04 03:31:24 -0400

    Added private member data and get and se functions for it

--- Skateboard.h
+++ Skateboard.h
@@ -1,11 +1,17 @@
+// Vinson Thomas 
 #ifndef DRIVINGSIMULATOR_SKATEBOARD_H
 #define DRIVINGSIMULATOR_SKATEBOARD_H
 
 #include "Vehicle.h"
 class Skateboard : public Vehicle
 {
-	public:
-	explicit Skateboard(string brand, string model); 
+private:
+	string typeOfWood;
+	
+public:
+	explicit Skateboard(string brand, string model, string wood); 
+	string getBoardWood();
+	void setBoardWood(string wood);
 	virtual ~Skateboard(); 
 	virtual double mileageEstimate(double time); 
 	virtual string toString();
