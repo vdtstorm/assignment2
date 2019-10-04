bd3e5d79dad3090435c0e2e99dec89f4070431ed
Vinson Thomas <vdt16@my.fsu.edu>
2019-10-04 03:12:57 -0400

    Added test cases for the Jet, Skateboard, and Semi classes

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include "Car.h"
 #include "Bicycle.h"
-
+#include "Semi.h"
+#include "Jet.h"
+#include "Skateboard.h"
 void printVehiclesRoster(Vehicle **vehicles, int size);
 
 int main() {
     std::cout << "Driving simulator" << std::endl;
-    int size = 6;
-    int capacity = 10;
+    int size = 12;
+    int capacity = 15;
     Vehicle **vehiclesArray = new Vehicle *[capacity];
 
     vehiclesArray[0] = new Car();
@@ -16,6 +18,14 @@ int main() {
     vehiclesArray[3] = new Car("Tesla", "T2", "electricity", "large");
     vehiclesArray[4] = new Bicycle("Mizuno", "Wave", 10);
     vehiclesArray[5] = new Car("BMW", "X5", "diesel", "grande");
+	vehiclesArray[6] = new Semi("MAC10", "X5", "diesel", "grande"); // adding test cases
+	vehiclesArray[7] = new Semi("Volvo", "X8", "Disel", "Really big");
+	vehiclesArray[8] = new Skateboard("Santa Cruz", "Baker Kader");
+	vehiclesArray[9] = new Skateboard("Zero Skateboards", "Baker");
+	vehiclesArray[10] = new Jet("Learjet", "31", "diesel", 7);
+	vehiclesArray[11] = new Jet("Hawker", "400", "Rocket", 10);
+
+
 
     printVehiclesRoster(vehiclesArray, size);
 
