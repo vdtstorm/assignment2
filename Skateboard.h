cbf0b62a917ffff723b86d08a17e44521963db12
Vinson Thomas <vdt16@my.fsu.edu>
2019-10-04 01:46:37 -0400

    Creation of declared funcions for header file of Skateboard

---
+++ Skateboard.h
@@ -0,0 +1,16 @@
+#ifndef DRIVINGSIMULATOR_SKATEBOARD_H
+#define DRIVINGSIMULATOR_SKATEBOARD_H
+
+#include "Vehicle.h"
+class Skateboard : public vehicle
+{
+	public:
+	explicit Skateboard(string brand, string model); 
+	virtual ~Skateboard(); 
+	virtual double mileageEstimate(double time); 
+	virtual string toString();
+};
+
+
+
+#endif
