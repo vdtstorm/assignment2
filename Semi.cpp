302ac2acbd6915db529c509da1ed216140f64a7a
Vinson Thomas <vdt16@my.fsu.edu>
2019-10-04 02:49:45 -0400

    Added function implementation for Semi,cpp

---
+++ Semi.cpp
@@ -0,0 +1,42 @@
+#include "Semi.h"
+
+Semi::Semi() 
+{
+    myEngineSize = "unknown";
+    setBrand("Custom");
+    setModel("MAC");
+}
+
+Semi::Semi(string brand, string model, string fuelType, string engineSize) 
+{
+    setBrand(brand);
+    setModel(model);
+    setFuelType(fuelType);
+    setEngineSize(engineSize);
+}
+
+Semi::~Semi() = default;
+
+string Semi::getEngineSize() 
+{
+    return myEngineSize;
+}
+
+void Semi::setEngineSize(string engineSize) 
+{
+    myEngineSize = engineSize;
+
+}
+
+double Car::mileageEstimate(double time) 
+{
+    double mileage = 8 * time; // average mpg for semi's
+  
+    return mileage;
+}
+
+string Car::toString() 
+{
+    return "-> Semi\n" + PoweredVehicle::toString() + "\n\tEngine Size: " +
+           getEngineSize();
+}
